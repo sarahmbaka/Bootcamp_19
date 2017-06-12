31becdf14a5b61199c038aa92e37d7ecda147dc3
sarahmbaka <salliedailey@gmail.com>
2017-06-12 21:34:53 +0300

    added comments to primechecker

--- prime_numbers/test_prime_number.py
+++ prime_numbers/test_prime_number.py
@@ -1,8 +1,19 @@
 import unittest
 from prime_number import get_prime_numbers
 
+"""
+test_it_works confirms if the function gives the correct output
 
-class Test_Generate_prime_numbers(unittest.TestCase):
+test_if_is_negative confirms if the number provided in a negative returns an error message
+
+test_if_is_zero confirms if the number entered in zero and returns an error message
+
+test_if_not_variable_type confirms if the variale type is int
+
+test_if_list confirms if the output of the function is a list
+
+"""
+class Test_prime_numbers(unittest.TestCase):
     def test_it_works(self):
         self.assertEqual(get_prime_numbers(43), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43])
 
