df70f11dfdee58fc4cd07b593b41bda0146b9274
sarahmbaka <salliedailey@gmail.com>
2017-06-12 21:54:13 +0300

    changed variable names in tests

--- prime_numbers/test_prime_number.py
+++ prime_numbers/test_prime_number.py
@@ -17,10 +17,10 @@ class Test_prime_numbers(unittest.TestCase):
     def test_it_works(self):
         self.assertEqual(get_prime_numbers(43), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43])
 
-    def test_if_is_negative(self):
+    def test_if_negative(self):
         self.assertEqual(get_prime_numbers(-10), 'Negatives or Zero not allowed')
 
-    def test_if_is_zero(self):
+    def test_if_zero(self):
         self.assertEqual(get_prime_numbers(0), 'Negatives or Zero not allowed')
 
     def test_if_not_variable_type(self):
